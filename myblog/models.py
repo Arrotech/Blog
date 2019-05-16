ffbadddb44760b1912f7cdc191196b5cfee3b8aa
Arrotech <arrotechdesign@gmail.com>
2019-05-16 13:21:07 +0300

    [Finishes #166056280] Add content to the blogging site.

--- myblog/models.py
+++ myblog/models.py
@@ -4,12 +4,19 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
+
 class Post(models.Model):
     """Create a new blog."""
 
     STATUS_CHOICES = (
         ('draft', 'Draft'),
-        ('published', 'Published')
+        ('published', 'Published'),
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique_for_date='publish')
@@ -21,13 +28,18 @@ class Post(models.Model):
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    def get_absolute_url(self):
-        """canonial url fro the model."""
-        return reverse('myblog:post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
-
-
-class Meta:
-    ordering = ('-publish',)
+    class Meta:
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+def get_absolute_url(self):
+    return reverse('myblog:post_detail',
+                   args=[self.publish.year,
+                         self.publish.month,
+                         self.publish.day,
+                         self.slug])
